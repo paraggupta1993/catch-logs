4b1bd8c7962c9445cca51dcef14740112bd36a67
Parag Gupta <paraggupta1993@gmail.com>
2014-09-26 14:53:16 +0530

    Basic searching : REgex support for search

--- public/javascripts/getLogs.js
+++ public/javascripts/getLogs.js
@@ -1,9 +1,39 @@
 (function(){
   var PORT = 4000;
+  var terminateEvent = function(event){
+    if(event.stopPropagation)
+      event.stopPropagation();   // Stops Bubbling up to Ancestor elements
+    if(event.preventDefault)
+      event.preventDefault();    // Stops the default action Eg : Redirecting to a "href" for anchor element
+    return false;
+  };
 
   var bindEvents = function(){
     $('.clearLogBtn').click(function(e){
       $('.container.logs').empty();
+    });
+
+    $('#regex').keyup(function(e){
+      var searchVal = $(e.target).val();
+      if(searchVal == '') {
+        console.log('empty search');
+        $('.log_line').show();
+      }
+      else {
+        re = new RegExp(searchVal);
+        $('.log_line').each(function(index, log){
+          match = re.exec(log.innerHTML);
+          if(match){
+            //Add divs to make the matched part of different color
+            //console.log(match.index + match.length);
+            $(log).show();
+          }
+          else{
+            $(log).hide();
+          }
+        });
+      }
+      if(e.stopPropagation) e.stopPropagation();   // Stops Bubbling up to Ancestor elements
     });
   };
 
@@ -23,7 +53,7 @@
 
     // Appends new-data into the container
     socket.on('new-data', function(data){
-      var newItem = $('<div>' + data.value + '</div>');
+      var newItem = $('<div class="log_line">' + data.value + '</div>');
       container.append(newItem);
     });
   };
